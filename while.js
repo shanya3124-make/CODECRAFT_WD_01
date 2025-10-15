e1a6f9e29c10379cbb0e3bfb675ba31a4015e7cd
shanya3124-make <shanya3124@gmail.com>
2025-10-15 22:10:16 +0530

    Updated code

---
+++ while.js
@@ -0,0 +1,11 @@
+let i=1;
+while(i<=5){
+    document.getElementById("loop").innerHTML+=i+"<br>";
+    i=i+1;
+}
+let j=6;
+do{
+    document.getElementById("loop").innerHTML+=j+"<br>";
+    j=j+1;
+}
+while(j<=10);
